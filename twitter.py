ca7c0d9161be04aa24aaae4031e79e6bde474f76
TanguyMrtn <tanguy.mariton38@gmail.com>
2020-04-18 11:54:35 +0200

    viz added

--- twitter.py
+++ twitter.py
@@ -9,31 +9,31 @@ import requests
 import tweepy
 import json
 
-def createIndex(indexName):
+def createIndex(url,indexName):
     """
-    Cette fonction permet de créer un index "indexName" dans elasticsearch
+    Cette fonction permet de créer un index "indexName" dans elasticsearch à l'url "url"
     """
     
     request = url+"/"+indexName
     response = requests.put(request)
     print(response)
 
-def createMapping(indexName, maping = None):
+def createMapping(url,indexName, mapping = None):
     """
-    Cette fonction permet de créer le mapping sur l'index "indexName"
+    Cette fonction permet de créer le mapping sur l'index "indexName" à l'url "url"
     """
     headers = {
     'Content-type': 'application/json',
     }
     request = url+"/"+indexName+"/_mapping"
-    if maping == None :    
-        maping = '{"properties":{"language":{"type":"text"},"text":{"type":"text"},"userName":{"type":"text"}}}'
-    response = requests.put(request,headers=headers,data=maping)
+    if mapping == None :    
+        mapping = '{"properties":{"language":{"type":"keyword"},"text":{"type":"text"},"userName":{"type":"text"},"platform":{"type":"keyword"},"hashtags":{"type":"keyword"}}}'
+    response = requests.put(request,headers=headers,data=mapping)
     print(response)
 
-def addRecord(indexName,data):
+def addRecord(url,indexName,data):
     """
-    Cette fonction permet d'ajouter un document "data" dans l'index "indexName"
+    Cette fonction permet d'ajouter un document "data" dans l'index "indexName" à l'url "url"
     """
     headers = {
     'Content-type': 'application/json',
@@ -44,6 +44,12 @@ def addRecord(indexName,data):
 
 class MyStreamListener(tweepy.StreamListener):
     
+    def setIndexName(self,indexName):
+        self.indexName = indexName
+    
+    def setUrl(self,url):
+        self.url = url
+    
     def on_status(self, status):
         """
         Pour chaque tweet du stream, on va récupérer la langue, le contenu et le nom de l'auteur puis on
@@ -52,30 +58,37 @@ class MyStreamListener(tweepy.StreamListener):
         langage = status.lang
         text = status.text
         userName = status.user.name
-        doc={"langage":langage,"text":text,"userName":userName}
-        addRecord("twitter",doc)
-
-
+        platform = status.source_url
+        hashtags=[]
+        for hashtagDoc in status.entities["hashtags"]:
+            hashtag = hashtagDoc["text"].lower()
+            hashtags.append(hashtag)
+        doc={"langage":langage,"text":text,"userName":userName,"platform":platform,"hashtags":hashtags}
+        addRecord(self.url,self.indexName,doc)
 
 # Votre url elasticsearch
-url = "https://elastic:votreMdp@votreAdresse:9243"
+elasticUrl = "https://elastic:votreMdp@votreAdresse:9243"
 
 # Tokens d'authentification
 auth = tweepy.OAuthHandler("votreConsumerKey", "votreConsumerSecret")
 auth.set_access_token("votreAccessToken", "votreAccessTokenSecret")
 
+# On initialise notre stream listener, en y ajoutant l'index dans lequel on veut ajouter les documents
+myListener = MyStreamListener()
+myListener.setIndexName("twitter")
+myListener.setUrl(elasticUrl)
+
 # On initialise le stream
-myStream = tweepy.Stream(auth = auth, listener=MyStreamListener())
+myStream = tweepy.Stream(auth = auth, listener=myListener)
 
 # On créé l'index et on y associe le mapping
 
-createIndex("twitter") # Vérifier via l'affichage d'un code 200 et dans dev tools avec : GET _cat/indices. Si index existe déjà, ne pas rerun.
-createMapping("twitter") # Vérifier via l'affichage d'un code 200 et dans dev tools avec : GET twitter/_mapping. Si mapping existe déjà, ne pas rerun.
+createIndex(elasticUrl,"twitter") # Vérifier via l'affichage d'un code 200 et dans dev tools avec : GET _cat/indices. Si index existe déjà, ne pas rerun.
+createMapping(elasticUrl,"twitter") # Vérifier via l'affichage d'un code 200 et dans dev tools avec : GET twitter/_mapping. Si mapping existe déjà, ne pas rerun.
+
 
 # On stream les tweets ayant un mot clé coronavirus et dans la langue française
-myStream.filter(track=['coronavirus'],languages=['fr']) # Vérifier via l'affichage d'un code 201 et dans dev tools avec GET twitter/_search
-
-
+myStream.filter(track=['coronavirus','covid','covid19'],languages=['en','fr']) # Vérifier via l'affichage d'un code 201 et dans dev tools avec GET twitter/_search
 
 
 
